5ab61f01519a8e12d1649bea9002c28ca622bae3
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2024-10-24 21:18:48 +0530

    song card file created and updates

--- nightway/src/components/SongCard.jsx
+++ nightway/src/components/SongCard.jsx
@@ -1,12 +1,14 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View, } from 'react-native'
 import React from 'react'
 
 
 const imageUrl="https://linkstorage.linkfire.com/medialinks/images/300f46c8-b0a0-4cea-a953-1e47342da5ca/artwork-440x440.jpg"
 const SongCard = () => {
   return (
-    <View>
-          <Image source={{ uri: imageUrl }} style={styles.coverImage } />
+      <View style={styles.container}>
+          <Image source={{ uri: imageUrl }} style={styles.coverImage} />
+          <Text>Faster</Text>
+          <Text>Progressive House</Text>
     </View>
   )
 }
@@ -15,9 +17,13 @@ const SongCard = () => {
 export default SongCard
 
 const styles = StyleSheet.create({
+    container: {
+        height:250
+    },
     coverImage: {
-        height: 100,
-        width: 100,
-        borderRadius:10,
+        height: 250,
+        width: 250,
+        borderRadius:10
+         
     }
 })
