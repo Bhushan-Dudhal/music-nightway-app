d3eb073e3cf8ea4452f1527060651350e14287a9
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2024-11-26 15:44:18 +0530

    likescreeen  FloatingPlayear  file added   updated

--- nightway/src/screen/LikeScreen.jsx
+++ nightway/src/screen/LikeScreen.jsx
@@ -5,6 +5,7 @@ import AntDesign from 'react-native-vector-icons/AntDesign'
 import { FontSize, iconSizes, spacing } from '../constants/dimensions'
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
 import SongCard from '../components/SongCard'
+import FloatingPlayear from '../components/FloatingPlayear'
 
 
 
@@ -24,8 +25,12 @@ const LikeScreen = () => {
 
               </TouchableOpacity>
           </View>
-          <Text style={styles.headingText}>Liked Songs</Text>
-          <FlatList data={[1, 2, 3, 4, 5, 6, 7, 8, 9]} renderItem={() => <SongCard containerStyle={{ width: "47%" }} imageStyle={{
+          <FlatList
+              ListHeaderComponent={
+                  <Text style={styles.headingText}>Liked Songs</Text>
+                  
+             }
+              data={[1, 2, 3, 4, 5, 6, 7, 8, 9,10]} renderItem={() => <SongCard containerStyle={{ width: "47%" }} imageStyle={{
               height: 160,
               width: 160,
           }} />}
@@ -39,6 +44,7 @@ const LikeScreen = () => {
                   marginVertical:spacing.lg
               }}
           />
+          <FloatingPlayear/>
     </View>
   )
 }
@@ -54,8 +60,9 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        paddingTop: spacing.md,
-        paddingHorizontal:spacing.md
+        padding:spacing.md
+        // paddingVertical: spacing.md,
+        // paddingHorizontal:spacing.md
     },
     headingText: {
         fontSize: FontSize.xl,
